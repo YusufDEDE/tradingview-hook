d1578ee0a26f483b09561e9bfd220dcc7b155ac5
Yusuf Dede <yusufdede95@hotmail.com>
2021-09-02 14:23:49 +0300

    update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -6,9 +6,9 @@ celery = Celery(CELERY_NAME, broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
 
 @celery.task(name='celery_worker.send_telegram', bind=True)
-def send_telegram(self, kwargs):
+def send_telegram(self, task_data):
     telegram_signal_send(
-        chat_id=kwargs.get('chat_id'),
-        api_key=kwargs.get('api_key'),
-        text=kwargs.get('text')
+        chat_id=task_data.get('chat_id'),
+        api_key=task_data.get('api_key'),
+        text=task_data.get('text')
     )
