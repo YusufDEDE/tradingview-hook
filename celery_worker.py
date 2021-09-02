6eaa8cb5c04f4fa977fe963591b3972de8395751
Yusuf Dede <yusufdede95@hotmail.com>
2021-09-02 14:46:22 +0300

    chat_id arr multi send

--- celery_worker.py
+++ celery_worker.py
@@ -7,8 +7,11 @@ celery = Celery(CELERY_NAME, broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
 @celery.task(name='celery_worker.send_telegram', bind=True)
 def send_telegram(self, task_data):
-    telegram_signal_send(
-        chat_id=task_data.get('chat_id'),
-        api_key=task_data.get('api_key'),
-        text=task_data.get('text')
-    )
+    chat_id_arr = ['-1001449574682', '-1001590879734']
+
+    for chat_id in chat_id_arr:
+        telegram_signal_send(
+            chat_id=chat_id,
+            api_key=task_data.get('api_key'),
+            text=task_data.get('text')
+        )
